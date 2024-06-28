6e80880e8c50fb1624cd93cdadf158ed0b26475b
RoronoaZoroDAce <90967650+RoronoaZoroDAce@users.noreply.github.com>
2024-06-28 16:26:05 +0530

    added footer

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 // import React from 'react'
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
+import Button from './Button';
+import styles from '../style';
+
+const CTA = () =>  (
+
+
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex flex-col flex-1">
+        <h2 className={`${styles.heading2}`}>Let's try our service now! </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>Everything you need to accept card payments and grow your business anywhere on the planet
+          </p>
+       
+      </div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
   )
-}
+
 
 export default CTA;
